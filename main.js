005ed4ba0c7a2a32925853a74098196670bd257c
August <hello@aughost.codes>
2021-05-15 00:13:40 -0400

    Just a button

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log('test');
+'use strict';
+
+const addGameBtn = document.querySelector('.add-game-btn');
+
+addGameBtn.addEventListener('click', addGameToLibrary);
+
+function addGameToLibrary() {
+    console.log('added game in headcanon');
+}
