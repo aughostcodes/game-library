f9e3880ca371db6f78248342ee2c3af316853bd7
August <hello@aughost.codes>
2021-05-21 20:21:52 -0400

    Beginning modal stuff

--- main.js
+++ main.js
@@ -6,4 +6,10 @@ addGameBtn.addEventListener('click', addGameToLibrary);
 
 function addGameToLibrary() {
     console.log('added game in headcanon');
+    console.log(Object.create(Game.prototype));
+}
+
+function Game(name, platform) {
+    this.name = name;
+    this.platform = platform;
 }
